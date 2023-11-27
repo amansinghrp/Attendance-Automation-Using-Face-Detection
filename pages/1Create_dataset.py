e72565aace6a5ff6ce61b6b623f1653deb46eec0
Aman Singh <rajputaman6554@gmail.com>
2023-11-27 20:13:45 +0530

    In the create dataset page, added a toggle button to activate video capture or not

--- pages/1Create_dataset.py
+++ pages/1Create_dataset.py
@@ -11,59 +11,62 @@ def assure_path_exists(path):
     dir = os.path.dirname(path)
     if not os.path.exists(dir):
         os.makedirs(dir)
-        
-#takin user id input
-face_id = st.text_input("Enter your ID:")
+activated = st.toggle("Start Capturing")
+if activated:
+    #takin user id input
+    face_id = st.text_input("Enter your ID:")
 
-# Start capturing video
-vid_cam = cv2.VideoCapture(0)
-st.write("Capturing")
+    # Start capturing video
+    vid_cam = cv2.VideoCapture(0)
+    st.write("Capturing")
 
-# Detect object in video stream using Haarcascade Frontal Face
-face_detector = cv2.CascadeClassifier('C:/Users/Lenovo/AppData/Local/Programs/Python/Python311/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
+    # Detect object in video stream using Haarcascade Frontal Face
+    face_detector = cv2.CascadeClassifier('C:/Users/Lenovo/AppData/Local/Programs/Python/Python311/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
 
-# Initialize sample face image
-count = 0
+    # Initialize sample face image
+    count = 0
 
-assure_path_exists("D:/Study/Coding/Projects/Mini Project/main/Dataset/")
+    assure_path_exists("D:/Study/Coding/Projects/Mini Project/main/Dataset/")
 
-# Start looping
-while (True):
+    # Start looping
+    while (True):
 
-    # Capture video frame
-    _, image_frame = vid_cam.read()
+        # Capture video frame
+        _, image_frame = vid_cam.read()
 
-    # Convert frame to grayscale
-    gray = cv2.cvtColor(image_frame, cv2.COLOR_BGR2GRAY)
+        # Convert frame to grayscale
+        gray = cv2.cvtColor(image_frame, cv2.COLOR_BGR2GRAY)
 
-    # Detect frames of different sizes, list of faces rectangles
-    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+        # Detect frames of different sizes, list of faces rectangles
+        faces = face_detector.detectMultiScale(gray, 1.3, 5)
 
-    # Loops for each faces
-    for (x, y, w, h) in faces:
-        # Crop the image frame into rectangle
-        cv2.rectangle(image_frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        # Loops for each faces
+        for (x, y, w, h) in faces:
+            # Crop the image frame into rectangle
+            cv2.rectangle(image_frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-        # Increment sample face image
-        count += 1
+            # Increment sample face image
+            count += 1
 
-        # Save the captured image into the datasets folder
-        cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y + h, x:x + w])
+            # Save the captured image into the datasets folder
+            cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y + h, x:x + w])
 
-        # Display the video frame, with bounded rectangle on the person's face
-        cv2.imshow('frame', image_frame)
+            # Display the video frame, with bounded rectangle on the person's face
+            cv2.imshow('frame', image_frame)
 
-    # To stop taking video, press 'q' for at least 100ms
-    if cv2.waitKey(100) & 0xFF == ord('q'):
-        break
+        # To stop taking video, press 'q' for at least 100ms
+        if cv2.waitKey(100) & 0xFF == ord('q'):
+            break
 
-    # If image taken reach 100, stop taking video
-    elif count >= 50:
-        st.success("Successfully Captured")
-        break
+        # If image taken reach 100, stop taking video
+        elif count >= 50:
+            st.success("Successfully Captured")
+            break
 
-# Stop video
-vid_cam.release()
+    # Stop video
+    vid_cam.release()
 
-# Close all started windows
-cv2.destroyAllWindows()
+    # Close all started windows
+    cv2.destroyAllWindows()
+else:
+    st.info("Video capturing is not turned on")
